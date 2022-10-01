0e996c3bf185a82dd451a94fdad1c5f009935635
Max McDonough <max.mcdonough@gmail.com>
2022-09-30 21:44:32 -0400

    Added TypeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,33 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Profile {
+    type User {
         _id: ID
-        name: String
-        email: String
-        password: String
-        skills: [String]!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
     }
 
     type Query {
-        profiles: [Profile]!
-        profile(profileId: ID!): Profile
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+
+        addBook(userId: ID!, authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
+        removeBook(userId: ID!, authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
     }
 `;
 
